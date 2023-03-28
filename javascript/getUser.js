e36e7919d7d1a78e1f2dd5721c5c9ad598d16a4d
Nicolas Agromartin <nicolas.agromartin97@gmail.com>
2023-03-28 15:15:00 -0300

    correcciones solicitadas por el tutor

--- javascript/getUser.js
+++ javascript/getUser.js
@@ -1,49 +1,26 @@
-// variable 
-  // contiene los datos del usuario generado
-    var user = [];
-  // variable de los elementos html
-    const image = document.querySelector('.head-image');
-    const information = document.querySelector('.user-data-information');
-    const name = document.querySelector('.user-data-name')
-    // botones 
-    const locationDataButton = document.getElementById('LocationBtn');
-    const phoneDataButton = document.getElementById('PhoneBtn');
-    const mailDataButton = document.getElementById('MailBtn');
 
 
 
+// fake form submit
+const form = document.querySelector('#form');
+form.addEventListener('submit', (e) => e.preventDefault() )
 
 
 
+// esconder el header
+const header = document.querySelector('.header');
+var lastScrollTop = 0;
 
-
-locationDataButton.addEventListener('click', () => {
-  information.innerHTML = `${user.location.city}, ${user.location.state}, ${user.location.country}`
-})
-
-phoneDataButton.addEventListener('click', () => {
-  information.innerHTML = ` ${user.phone}`
-})
-
-mailDataButton.addEventListener('click', () => {
-  information.innerHTML = `${user.email}`
+window.addEventListener('scroll', () => {
+  document.documentElement.scrollTop > lastScrollTop ? (header.style.top = '-80px') : (header.style.top = '0')
+  lastScrollTop = document.documentElement.scrollTop;
 })
 
 
-function loadData(){
+// mostrar experiencia
+const expButton = document.querySelectorAll('.exp-button');
 
-  fetch(`https://randomuser.me/api/`)
-    .then(res => res.json())
-    .then(data => {
-      user = data.results[0];
-      console.log(user);
-      
-      image.src = `${data.results[0].picture.large}`;
-      name.innerHTML = `${data.results[0].name.title} ${data.results[0].name.first} ${data.results[0].name.last}`;
-      information.innerHTML = `${data.results[0].location.city}, ${data.results[0].location.state}, ${data.results[0].location.country}`
-    })
-    
-}
-
-
-loadData()
+expButton.forEach( button => button.addEventListener('click', () => {
+  document.querySelector('.exp-1').style.background = '#000'
+  })
+)
